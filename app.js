d52d9c63972e39a6f69d1c3ff207e07a97ff04e3
Ravi <ravi.raushan.nl@gmail.com>
2026-06-01 12:01:40 +0530

    Created: Nested HTML element using React

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const heading = React.createElement("h1", {id: "heading"}, "Namaste React Using React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {id: "innerHeading"}, "Inner Heading")));
+
+root.render(parent);
