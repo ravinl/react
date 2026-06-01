bceb1295005c960eac436a5d5612eff48db2d204
Ravi <ravi.raushan.nl@gmail.com>
2026-06-01 10:52:49 +0530

    Created: Separate js file for js code

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {id: "heading", color: "red"}, "Namaste React Using React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
